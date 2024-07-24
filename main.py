816005e81629adb6bd1d54b81fd59d1e9369fe1b
Ian Chege <ianchege001@gmail.com>
2024-07-24 20:32:12 +0300

    refactor: Refactor download function to use yt-dlp for improved performance and reliability

--- main.py
+++ main.py
@@ -1,46 +1,35 @@
-from pytube import YouTube, Playlist
 import csv
 import os
 import subprocess
 
 
 def merge_audio_and_video(video_file, audio_file, output_file):
-    # Construct the FFmpeg command
-    cmd = f'ffmpeg -i "{video_file}" -i "{audio_file}" -c:v copy -c:a aac -map 0:v:0 -map 1:a:0 "{output_file}"'
-
-    # Run the FFmpeg command
+    cmd = f'ffmpeg -i "{video_file}" -i "{
+        audio_file}" -c:v copy -c:a aac -map 0:v:0 -map 1:a:0 "{output_file}"'
     subprocess.call(cmd, shell=True)
 
 
 def Download(link):
-    youtubeObject = YouTube(link)
-    video_stream = youtubeObject.streams.filter(
-        progressive=False, file_extension='mp4').order_by('resolution').desc().first()
-    audio_stream = youtubeObject.streams.filter(only_audio=True).first()
+    cmd_video = f'yt-dlp -f "bestvideo[ext=mp4]" -o "video.mp4" {link}'
+    cmd_audio = f'yt-dlp -f "bestaudio[ext=m4a]" -o "audio.m4a" {link}'
 
-    video_file = video_stream.download(filename='video')
-    audio_file = audio_stream.download(filename='audio')
+    subprocess.call(cmd_video, shell=True)
+    subprocess.call(cmd_audio, shell=True)
 
-    # Replace invalid characters in the filename
-    output_file = f"{youtubeObject.title}.mp4"
-    for char in [' ', '|', '/', '\\', ':', '*', '?', '"', '<', '>']:
-        output_file = output_file.replace(char, '_')
+    output_file = f"output.mp4"
+    merge_audio_and_video("video.mp4", "audio.m4a", output_file)
 
-    merge_audio_and_video(video_file, audio_file, output_file)
-
-    os.remove(video_file)
-    os.remove(audio_file)
+    os.remove("video.mp4")
+    os.remove("audio.m4a")
 
     print("Download is completed successfully")
 
 
 def download_single_or_playlist(url):
     if "/watch?v=" in url:
-        # Single YouTube video URL
         print("Downloading single video:", url)
         Download(url)
     elif "/playlist?list=" in url:
-        # YouTube playlist URL
         print("Downloading playlist:", url)
         download_playlist(url)
     else:
@@ -48,13 +37,10 @@ def download_single_or_playlist(url):
 
 
 def download_playlist(playlist_link):
-    playlist = Playlist(playlist_link)
-    for video_url in playlist.video_urls:
-        print("Downloading:", video_url)
-        Download(video_url)
+    cmd = f'yt-dlp -i -o "%(title)s.%(ext)s" {playlist_link}'
+    subprocess.call(cmd, shell=True)
 
 
-# Read links from links.csv file
 with open("links.csv") as file:
     csvreader = csv.reader(file)
     for row in csvreader:
